ba58cc2d1af0aef33028d40dcda6a2d43c42d0b2
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-02-03 15:40:46 +0800

    Update advancedplus.lua

--- luci-app-advancedplus/luasrc/controller/advancedplus.lua
+++ luci-app-advancedplus/luasrc/controller/advancedplus.lua
@@ -9,10 +9,12 @@ function index()
 	end
 
 	local page
-	page = entry({"admin","system","advancedplus"},alias("admin","system","advancedplus","kucatset"),_("Advanced plus"),61)
+	page = entry({"admin","system","advancedplus"},alias("admin","system","advancedplus","advancededit"),_("Advanced plus"),61)
 	page.dependent = true
 	page.acl_depends = { "luci-app-advancedplus" }
+	if luci.sys.call("grep \"mediaurlbase '/luci-static/kucat'\" /etc/config/luci >/dev/null") == 0 then
 	entry({"admin","system","advancedplus","kucatset"},cbi("advancedplus/kucatset"),_("KuCat Theme Config"),20).leaf = true
+	end
 	entry({"admin","system","advancedplus","advancedset"},cbi("advancedplus/advancedset"),_("Advanced Setting"),10).leaf = true
 	entry({"admin","system","advancedplus","advancededit"},cbi("advancedplus/advancededit"),_("Advanced Edit"),60).leaf = true
 	entry({"admin", "system","advancedplus","upload"}, form("advancedplus/upload"), _("Login Background Upload"), 70).leaf = true
